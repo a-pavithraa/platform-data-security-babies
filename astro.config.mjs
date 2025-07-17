8e3e278c1d58a9f819f649986c2cd552433476b6
pavithra <a.pavithraa@gmail.com>
2025-07-17 14:42:01 +0530

    feat: Add GitHub Actions workflow for deploying to GitHub Pages and configure Astro site settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://cdgbabies.github.io',
+  base: '/platform-data-security-babies',
+});
